3af61be0cf7e93c4077e0f8dce78680e28ae837b
Jenfer <1634114623@qq.com>
2024-03-04 17:49:25 +0800

    整合Redission

--- src/main/java/com/jenfer/frentmatch/job/PreCacheJob.java
+++ src/main/java/com/jenfer/frentmatch/job/PreCacheJob.java
@@ -1,17 +1,22 @@
 package com.jenfer.frentmatch.job;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jenfer.frentmatch.model.domain.User;
 import com.jenfer.frentmatch.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -22,11 +27,35 @@ public class PreCacheJob {
     @Resource
     private RedisTemplate redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     private List<Long> mainUserList = Arrays.asList(1L);
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void doCacheRecommendUser(){
-        LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        RLock lock = redissonClient.getLock("redisson:lock:recommend:user");
+        try {
+            if(lock.tryLock(0,-1,TimeUnit.MILLISECONDS)){
+                LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<>();
+                Page<User> page = userService.page(new Page<User>(1, 10), userLambdaQueryWrapper);
+                String redisKey = String.format("main:recommend:user:%s", mainUserList);
+                ValueOperations valueOperations = redisTemplate.opsForValue();
+                try {
+                    valueOperations.set(redisKey,page,30000, TimeUnit.MILLISECONDS);
+                }catch (Exception e){
+                    log.error("缓存推荐用户失败", e);
+                }
+            }
+
+        }catch (Exception e){
+            log.error("获取redis锁失败", e);
+        }finally {
+            if (lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+
+        }
 
     }
 
--- src/test/java/com/jenfer/frentmatch/UserServiceTest.java
+++ src/test/java/com/jenfer/frentmatch/UserServiceTest.java
@@ -1,19 +1,31 @@
 package com.jenfer.frentmatch;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jenfer.frentmatch.model.domain.User;
 import com.jenfer.frentmatch.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
+@Slf4j
 public class UserServiceTest {
 
    @Resource
    private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    private List<Long> mainUserList = Arrays.asList(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L);
     @Test
     public void testSearchUserByTags() {
         List<String> list = Arrays.asList("大一");
@@ -21,6 +33,21 @@ public class UserServiceTest {
         System.out.println(users);
     }
 
+
+    @Test
+    public void testMainList(){
+        String redisKey = String.format("main:recommend:user:%s", mainUserList);
+        System.out.println(redisKey);
+        LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        Page<User> page = userService.page(new Page<User>(1, 10), userLambdaQueryWrapper);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+
+        try {
+            valueOperations.set(redisKey,page,30000, TimeUnit.MILLISECONDS);
+        }catch (Exception e){
+            log.error("缓存推荐用户失败", e);
+        }
+    }
 
 
 }
